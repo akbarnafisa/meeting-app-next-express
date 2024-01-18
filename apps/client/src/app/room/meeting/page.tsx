45a78ca136a07e02ea6ab1fbd35b478538efe08a
Akbar Nafisa <akbarnj788@gmail.com>
2024-01-18 14:31:08 +0700

    feat: handle redirect if socketId not found

--- apps/client/src/app/room/meeting/page.tsx
+++ apps/client/src/app/room/meeting/page.tsx
@@ -6,30 +6,36 @@ import { useSocket } from "@/lib/hooks/useSocket";
 import { getLocalPreviewAndRoomConnection } from "@/lib/rtc-handler";
 import useRoomStore from "@/store";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function JoinRoom() {
   const meetingStore = useRoomStore((state) => state);
   const socketContext = useSocket();
+  const router = useRouter();
 
   useEffect(() => {
     const initiateRoom = async () => {
-      meetingStore.setIsInitiateRoom(true);
+      if (!meetingStore.socketId) {
+        router.replace("/");
+      } else {
+        meetingStore.setIsInitiateRoom(true);
 
-      if (socketContext && socketContext.socket) {
-        await getLocalPreviewAndRoomConnection(
-          {
-            isConnectOnlyAudio: meetingStore.isConnectOnlyAudio,
-            meetingId: meetingStore.roomId,
-            meetingName: meetingStore.meetingName,
-            isHostMeeting: meetingStore.isHostMeeting,
-          },
-          meetingStore.socketId,
-          socketContext.socket
-        );
+        if (socketContext && socketContext.socket) {
+          await getLocalPreviewAndRoomConnection(
+            {
+              isConnectOnlyAudio: meetingStore.isConnectOnlyAudio,
+              meetingId: meetingStore.roomId,
+              meetingName: meetingStore.meetingName,
+              isHostMeeting: meetingStore.isHostMeeting,
+            },
+            meetingStore.socketId,
+            socketContext.socket
+          );
 
-        meetingStore.setIsInitiateRoom(false);
-        meetingStore.setMicrophone();
-        meetingStore.setVideo(!meetingStore.isConnectOnlyAudio);
+          meetingStore.setIsInitiateRoom(false);
+          meetingStore.setMicrophone();
+          meetingStore.setVideo(!meetingStore.isConnectOnlyAudio);
+        }
       }
     };
 
@@ -37,8 +43,6 @@ export default function JoinRoom() {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [socketContext]);
-
-
 
   return (
     <section className="min-h-screen flex flex-col overflow-hidden">
