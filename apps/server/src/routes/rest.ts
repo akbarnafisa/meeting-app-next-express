f25ba91b7c4f50b7a896c3e0ea241c0cc49c03e0
Akbar Nafisa <akbarnj788@gmail.com>
2024-01-19 13:43:16 +0700

    feat: add health API

--- apps/server/src/routes/rest.ts
+++ apps/server/src/routes/rest.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import { rtcController } from "../controller/rtc";
 
-
 const router = express.Router();
 
 router.get("/rtc/room/:id", rtcController);
-
+router.get("/health", (_, res, next) => {
+  try {
+    res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
