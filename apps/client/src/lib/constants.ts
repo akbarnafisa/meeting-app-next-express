1edbcdbf34a52af077a9702800c653943cdab253
Akbar Nafisa <akbarnj788@gmail.com>
2024-01-24 11:18:02 +0700

    build: update socket path

--- apps/client/src/lib/constants.ts
+++ apps/client/src/lib/constants.ts
@@ -1,1 +1,3 @@
-export const API_URL= process.env.NEXT_PUBLIC_ENV_API_URL
+export const API_URL= process.env.NEXT_PUBLIC_ENV_API_URL || ''
+export const SOCKET_PATH= process.env.NEXT_PUBLIC_ENV_SOCKET_PATH  || ''
+
