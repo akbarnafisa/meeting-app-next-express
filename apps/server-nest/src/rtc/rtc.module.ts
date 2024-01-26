ba76f03d429cbce7adbb690098bd75fb902d9648
Akbar Nafisa <akbarnj788@gmail.com>
2024-01-26 17:14:40 +0700

    feat: add rtcgateway

--- apps/server-nest/src/rtc/rtc.module.ts
+++ apps/server-nest/src/rtc/rtc.module.ts
@@ -3,10 +3,11 @@ import { RtcController } from './rtc.controller';
 import { RtcService } from './rtc.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { RtcRepository } from './rtc.repository';
+import { RtcGateway } from './rtc.gateway';
 
 @Module({
   controllers: [RtcController],
-  providers: [RtcService, RtcRepository],
+  providers: [RtcService, RtcRepository, RtcGateway],
   imports: [PrismaModule],
 })
 export class RtcModule {}
