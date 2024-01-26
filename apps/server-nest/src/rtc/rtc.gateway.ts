ed7f1963dffe478f610d98805648c16ea4ed8131
Akbar Nafisa <akbarnj788@gmail.com>
2024-01-26 13:40:52 +0700

    feat: add websocket emit

--- apps/server-nest/src/rtc/rtc.gateway.ts
+++ apps/server-nest/src/rtc/rtc.gateway.ts
@@ -2,16 +2,38 @@ import {
   WebSocketGateway,
   OnGatewayConnection,
   WebSocketServer,
+  OnGatewayDisconnect,
+  OnGatewayInit,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { RtcService } from './rtc.service';
 
 @WebSocketGateway()
-export class SocketGateway implements OnGatewayConnection {
+export class SocketGateway
+  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
+{
   @WebSocketServer()
   private server: Socket;
 
   constructor(private readonly socketService: RtcService) {}
 
-  handleConnection(socket: Socket): void {}
+  async handleConnectionInit(client: any): Promise<void> {
+    console.log('handleConnectionInit');
+    client.emit('on gateway connection');
+  }
+
+  async handleConnection(client: any): Promise<void> {
+    console.log('handleConnection');
+    client.emit('on gateway connection');
+  }
+
+  async afterInit(client: any): Promise<void> {
+    console.log('afterInit');
+    client.emit('on gateway init');
+  }
+
+  async handleDisconnect(client: Socket): Promise<void> {
+    console.log('handleDisconnect');
+    client.emit('on gateway init');
+  }
 }
